ef4c07c196f315e43496ec69f753233f60e398bd
Csaba Gyuris <gycsaba@vasvari.hu>
2018-11-16 11:37:29 +0100

    Vezérlő osztály szöveges adatok kezelésére. Feldolgozandó fájl adatok.txt

---
+++ OOPControllerStringData/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ControllerStringDataProcessing
+{
+    class Controller
+    {
+        List<string> subjects;
+        public Controller()
+        {
+            subjects = new List<string>();
+        }
+        public void readFromFile()
+        {
+            StreamReader sr = new StreamReader("adatok.txt");
+            while(!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                subjects.Add(line);
+            }
+        }
+        public int getNumberOfSubject()
+        {
+            return subjects.Count();
+        }
+    }
+}
---
+++ OOPControllerStringData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerStringDataProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller c = new Controller();
+            c.readFromFile();
+            Console.WriteLine("Beolvasott adatok száma: " + c.getNumberOfSubject());
+            Console.ReadKey();
+        }
+    }
+}
